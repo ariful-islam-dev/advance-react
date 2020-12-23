01870d196c74a6aa2b4f7fda743a143601f63d9a
nodeAriful <ariful.node82@gmail.com>
2020-12-23 21:32:44 +0600

    simple react app

---
+++ src/components/dumb.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Dumb = (props) => {
+    return (
+        <div>
+            <h3>I am Dumb</h3>
+            {props.msg}
+        </div>
+    )
+}
+
+export default Dumb
