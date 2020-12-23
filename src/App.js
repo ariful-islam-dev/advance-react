0e6da9ea217b63eb2ff02691b125c0ab6ab9717e
nodeAriful <ariful.node82@gmail.com>
2020-12-23 22:58:16 +0600

    Freagment is done

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
-import Smart from './components/smart';
+import React, { Fragment } from "react";
+import "./App.css";
 
-function App() {
+const Wrapper = ({children})=> children
+
+function Demo() {
+  return (
+    <Fragment>
+      <h1>Hello</h1>
+      <p>
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book. It has survived not only five
+        centuries, but also the leap into electronic typesetting, remaining
+        essentially unchanged. It was popularised in the 1960s with the release
+        of Letraset sheets containing Lorem Ipsum passages, and more recently
+        with desktop publishing software like Aldus PageMaker including versions
+        of Lorem Ipsum.
+      </p>
+    </Fragment>
+  );
+}
+
+const App = (props) => {
   return (
     <div className="App">
       <header className="App-header">
-          <h2>I am app component</h2>
-          <Smart/>
-             </header>
-    </div>
+        <h1>Welcome to React Advance Course</h1>
+        <Demo/>
+      </header>
+      </div>
+    
   );
-}
+};
 
 export default App;
